c12f7da2a4c10f6c83de732b82518ec46e908884
li33114413 <37331181+li33114413@users.noreply.github.com>
2022-07-21 12:24:40 +0800

    Add files via upload

---
+++ daemon.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+filename="/etc/docker/daemon.json"
+cat>"${filename}"<<EOF
+{
+   "default-address-pools": [
+        {
+            "base":"172.17.0.0/12",
+            "size":16
+        },
+        {
+            "base":"192.168.0.0/16",
+            "size":20
+        },
+        {
+            "base":"10.99.0.0/16",
+            "size":24
+        }
+    ]
+}
+EOF
